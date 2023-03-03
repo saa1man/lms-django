bae9ba51b94ce2b3affa719c35f88ac4a25d5580
Saal <salman.alilouch@gmail.com>
2023-03-04 00:09:02 +0100

    Updating 'Add Book' Form

--- lms_app/forms.py
+++ lms_app/forms.py
@@ -4,7 +4,31 @@ from .models import *
 class BookForm(forms.ModelForm):
     class Meta:
         model=Book
-        fields = '__all__'
+        fields = [
+            'title',
+            'author',
+            'photo_book',
+            'photo_author',
+            'pages',
+            'price',
+            'rental_price_day',
+            'rental_period',
+            'status',
+            'category',
+        ]
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'photo_book': forms.FileInput(attrs={'class': 'form-control'}),
+            'photo_author': forms.FileInput(attrs={'class': 'form-control'}),
+            'pages': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'rental_price_day': forms.NumberInput(attrs={'class': 'form-control'}),
+            'rental_period': forms.NumberInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
 
 
 
